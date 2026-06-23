a003778a7c61f22c6414c86214284961c6de38c5
Pankhuri Verma <vermapankhuri202429@gmail.com>
2026-06-24 02:33:18 +0530

    Add Cucumber TestRunner class with configuration for feature files and reporting

--- automation_project/src/test/java/cucumberOptions/TestRunner.java
+++ automation_project/src/test/java/cucumberOptions/TestRunner.java
@@ -1,5 +1,22 @@
 package cucumberOptions;
 
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"hooks", "stepDefinitions"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html"
+        },
+        monochrome = true,
+        publish = true
+)
+
 public class TestRunner {
 
 }
