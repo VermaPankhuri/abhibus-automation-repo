58c36965b1b6f9f9498bd4d90a1614efed64a6b6
Pankhuri Verma <vermapankhuri202429@gmail.com>
2026-06-24 02:28:33 +0530

    hooks created

--- automation_project/src/test/java/hooks/ScenarioHook.java
+++ automation_project/src/test/java/hooks/ScenarioHook.java
@@ -1,5 +1,58 @@
 package hooks;
 
-public class ScenarioHook {
+import java.net.URL;
+import java.time.Duration;
 
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import base.Base;
+import io.cucumber.java.Before;
+
+public class ScenarioHook extends Base {
+
+    RemoteWebDriver remoteDriver;
+
+    @Before
+    public void setup() {
+
+        try {
+            String browser = this.getBrowser();
+
+            // Chrome Browser Setup
+            if (browser.equals("chrome")) {
+
+                ChromeOptions options = new ChromeOptions();
+                options.setCapability("platformName", "Windows");
+
+                remoteDriver = new RemoteWebDriver(
+                        new URL("http://localhost:4444"),
+                        options
+                );
+            }
+
+            // Edge Browser Setup
+            else if (browser.equals("edge")) {
+
+                EdgeOptions options = new EdgeOptions();
+                options.setCapability("platformName", "Windows");
+
+                remoteDriver = new RemoteWebDriver(
+                        new URL("http://localhost:4444"),
+                        options
+                );
+            }
+
+            // Set Driver in Base class
+            setDriver(remoteDriver);
+
+            // Implicit Wait
+            getDriver().manage().timeouts()
+                    .implicitlyWait(Duration.ofSeconds(10));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
