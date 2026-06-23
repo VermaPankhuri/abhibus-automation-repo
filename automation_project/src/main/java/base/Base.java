07de64e18eeee5e77d02035897492a6fa553d36a
Pankhuri Verma <vermapankhuri202429@gmail.com>
2026-06-24 02:24:36 +0530

    Add Base class with WebDriver management and URL/browser retrieval methods; create TestRunner and AbhiBusPage classes; remove obsolete AppTest class

--- automation_project/src/main/java/base/Base.java
+++ automation_project/src/main/java/base/Base.java
@@ -1,10 +1,49 @@
 package base;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+import org.openqa.selenium.WebDriver;
+
 public class Base {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws Exception {
+        Base base = new Base();
 
-	}
+        System.out.println(base.getUrl());
+        System.out.println(base.getBrowser());
+    }
 
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void setDriver(WebDriver driver1) {
+        driver = driver1;
+    }
+
+    public String getUrl() throws Exception {
+        Properties prop = new Properties();
+
+        FileInputStream fis = new FileInputStream(
+                "path_of_properties_file");
+
+        prop.load(fis);
+
+        String url = prop.getProperty("url");
+        return url;
+    }
+
+    public String getBrowser() throws Exception {
+        Properties prop = new Properties();
+
+        FileInputStream fis = new FileInputStream(
+                "path_of_properties_file");
+
+        prop.load(fis);
+
+        String browser = prop.getProperty("browser");
+        return browser;
+    }
 }
